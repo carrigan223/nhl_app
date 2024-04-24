17812d6eda9b0faaf55513f3b51f4b05172b2512
Andrew <andrew@ripemetrics.com>
2024-04-23 23:30:35 -0700

    Update page.tsx to display team data and add TeamSummaryCard component

--- app/team/[id]/page.tsx
+++ app/team/[id]/page.tsx
@@ -2,22 +2,75 @@ import {
   retrieveAllStats,
   retrieveCurrentSeasonSummary,
 } from "@/app/components/data/retrieveCurrentSeasonSummary";
-import type { Team } from "@/app/types/team";
+import TeamSummaryCard from "@/app/components/summary/TeamSummaryCard";
+import type { Team, TeamStats } from "@/app/types/team";
 import { parse } from "path";
 import React from "react";
 
 type Props = {};
 
 const Team = async ({ params }: { params: { id: string } }) => {
+  const convertSeasonIdToYear = (seasonId: number) => {
+    const seasonIdString = seasonId.toString();
+    return `${seasonIdString.slice(0, 4)}/${seasonIdString.slice(4, 8)}`;
+  };
   const teamId = parseInt(params.id);
   const teamData = (await retrieveCurrentSeasonSummary(teamId)) as Team;
   const allStats = await retrieveAllStats(teamId);
-
+  console.log(teamData);
   return (
     <div>
-      <div>{params.id}</div>
-      <div>{JSON.stringify(teamData)}</div>
-      <div>{JSON.stringify(allStats)}</div>
+      <div
+        style={{
+          color: teamData.colors[0],
+        }}
+        className="text-xl font-bold w-full"
+      >
+        {teamData.name}
+      </div>
+      <span className="text-center">Historical Season Stats</span>
+      <div className="flex flex-wrap w-full ">
+        {allStats.map((stats: TeamStats, index: number) => {
+          return (
+            <div className="border m-2" key={stats.seasonId}>
+              <div
+                style={{
+                  backgroundColor: teamData.colors[0],
+                }}
+                className="w-full justify-center text-center items-center "
+              >
+                {convertSeasonIdToYear(stats.seasonId)}
+              </div>
+              <div>
+                {Object.keys(stats).map((key) => {
+                  const keysToIgnore = [
+                    "wins",
+                    "losses",
+                    "otLosses",
+                    "gamesPlayed",
+                    "ties",
+                    "teamId",
+                    "teamFullName",
+                    "seasonId",
+                  ];
+                  if (keysToIgnore.includes(key)) return null;
+                  //@ts-ignore
+                  const value = stats[key] as string | number;
+                  key = key.replace(/([A-Z])/g, " $1");
+                  key = key.charAt(0).toUpperCase() + key.slice(1);
+                  return (
+                    <div className="p-2" key={key}>
+                      <span className="font-bold">
+                        {key}: {value}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
